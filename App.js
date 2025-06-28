a47eba4064283592789b93d3f1648f7b5c0317fb
SHRMedia1776 <sackheadsradio@gmail.com>
2025-06-28 09:00:22 -0700

    fresh working
    fresh working

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-8 font-sans">
+      <h1 className="text-3xl font-bold mb-4">Welcome to SHR Media</h1>
+      <p>This is your starter React app with Tailwind CSS enabled.</p>
+    </div>
+  );
+}
+
+export default App;
